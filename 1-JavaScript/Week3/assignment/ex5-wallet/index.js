83c2f872130e02b6e6b19363f0b04e005fcdc7cc
İsa Eren <isaeren.ie@gmail.com>
2024-12-11 11:04:32 +0100

    last changes

--- 1-JavaScript/Week3/assignment/ex5-wallet/index.js
+++ 1-JavaScript/Week3/assignment/ex5-wallet/index.js
@@ -1,5 +1,3 @@
-// Based on an example from: Philipp Beau (@ze_german)
-
 const eurosFormatter = new Intl.NumberFormat('nl-NL', {
   style: 'currency',
   currency: 'EUR',
@@ -26,8 +24,14 @@ function createWallet(name, cash = 0) {
         wallet.name
       }`
     );
+
     const withdrawnAmount = withdraw(amount);
-    wallet.deposit(withdrawnAmount);
+
+    if (withdrawnAmount > 0) {
+      wallet.deposit(withdrawnAmount);
+    } else {
+      console.log('Transfer failed due to insufficient funds.');
+    }
   }
 
   function reportBalance() {
@@ -59,57 +63,49 @@ walletJack.reportBalance();
 walletJoe.reportBalance();
 walletJane.reportBalance();
 
-// * End of exercise code
-
-/*******************************************************************************
- * TODO: Multiple choice: provide your answers by replacing `undefined` with the
- * TODO: letter corresponding to your choice, e.g.  answer: 'a'
- ******************************************************************************/
-// prettier-ignore
-// eslint-disable-next-line no-unused-vars
 const quiz = {
   q1: {
     question: 'At line 26, which variables are in the scope marked Closure?',
-    choices: { 
-      a: 'There is no scope marked Closure', 
-      b: 'cash, name', 
-      c: 'amount, this, wallet'
+    choices: {
+      a: 'There is no scope marked Closure',
+      b: 'cash, name',
+      c: 'amount, this, wallet',
     },
     answer: 'b',
   },
   q2: {
     question: 'What is in the Call Stack, from top to bottom?',
-    choices: { 
-      a: 'withdraw, anonymous', 
-      b: 'anonymous, transferInto', 
-      c: 'transferInto, anonymous' 
+    choices: {
+      a: 'withdraw, anonymous',
+      b: 'anonymous, transferInto',
+      c: 'transferInto, anonymous',
     },
-    answer: 'a',
+    answer: 'c',
   },
   q3: {
     question: 'What tooltip appears when hovering over the third debug button?',
-    choices: { 
-      a: 'Step into next function call', 
-      b: 'Step out of current function', 
-      c: 'Step' 
+    choices: {
+      a: 'Step into next function call',
+      b: 'Step out of current function',
+      c: 'Step',
     },
     answer: 'a',
   },
   q4: {
     question: 'What is displayed in the console?',
-    choices: { 
-      a: 'Transferring € 50,00 from Jack to Joe', 
-      b: 'Transferring € 50,00 from Jack to undefined', 
-      c: 'Transferring € 50,00 from Jack to Jane' 
+    choices: {
+      a: 'Transferring € 50,00 from Jack to Joe',
+      b: 'Transferring € 50,00 from Jack to undefined',
+      c: 'Transferring € 50,00 from Jack to Jane',
     },
     answer: 'a',
   },
   q5: {
     question: 'The owner of the wallet with insufficient funds is:',
-    choices: { 
-      a: 'Jack', 
-      b: 'Joe', 
-      c: 'Jane' 
+    choices: {
+      a: 'Jack',
+      b: 'Joe',
+      c: 'Jane',
     },
     answer: 'c',
   },
