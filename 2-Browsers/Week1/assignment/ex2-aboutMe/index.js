839ebd2dd552c581edc69e619d6404298838db48
Feras-ALdemashki <feras.aldemashki@hotmail.com>
2025-01-07 22:02:35 +0100

    Finished project for assignment week 1 Browsers

--- 2-Browsers/Week1/assignment/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/assignment/ex2-aboutMe/index.js
@@ -8,4 +8,12 @@ Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-B
 3. Look in the css file!
 ------------------------------------------------------------------------------*/
 
-// TODO add your JavaScript code here.
+// Step 1: Replace each span with your information
+document.getElementById('nickname').textContent = 'vayne';
+document.getElementById('fav-food').textContent = 'chicken';
+document.getElementById('hometown').textContent = 'syria';
+
+const listItems = document.querySelectorAll('li');
+listItems.forEach((li) => {
+  li.classList.add('list-item');
+});
