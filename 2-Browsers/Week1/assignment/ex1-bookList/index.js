f8e1720f3371a653c988bf4537a25257a9643e35
khiro963 <khiro9633@gmail.com>
2025-01-07 17:18:32 +0100

    week1 browsers

--- 2-Browsers/Week1/assignment/ex1-bookList/index.js
+++ 2-Browsers/Week1/assignment/ex1-bookList/index.js
@@ -1,25 +1,33 @@
-//cspell: disable
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-Browsers/Week1#exercise-1-the-book-list
 
-I'd like to display my three favorite books inside a nice webpage!
+  function createBookList(books) {
 
-1. Iterate through the array of books.
-2. For each book, create a `<p>`
-element with the book title and author.
-3. Use a `<ul>`  and `<li>` to display the books.
-4. Add an `<img>` to each book that links to a URL of the book cover.
-5. Change the style of the book depending on whether you have read it(green) or not(red).
+    const bookList = document.createElement('ul');
 
-The end result should look something like this:
-https://hackyourfuture.github.io/example-pages/Browsers/Week1/1-booklist/
+    books.forEach(book => {
+  const bookItem = document.createElement('li');
+  bookItem.className= 'book';
 
------------------------------------------------------------------------------*/
-//cspell: enable
+  const bookInfo = document.createElement ('p');
+  bookInfo.textContent = `${book.title} by ${book.author} `;
+  bookItem.appendChild(bookInfo);
 
-function createBookList(books) {
-  // TODO your code goes in here, return the ul element
-}
+  const bookImg = document.createElement('img');
+  bookImg.src = book.imgSrc;
+  bookImg.alt =`${book.title}`;
+  bookItem.appendChild(bookImg)
+
+  if (book.alreadyRead){
+    bookItem.style.backgroundColor = 'green';
+  }else { 
+    bookItem.style.backgroundColor = 'red';
+  }
+  
+  bookList.appendChild(bookItem)
+
+
+  });
+    return bookList;
+  }
 
 function main() {
   const myBooks = [
@@ -27,19 +35,22 @@ function main() {
       title: 'The Design of Everyday Things',
       author: 'Don Norman',
       isbn: '978-0465050659',
-      alreadyRead: false,
+      alreadyRead: false, 
+      imgSrc : 'assets/the_design_of_everyday_things.jpg',
     },
     {
       title: 'The Most Human Human',
       author: 'Brian Christian',
       isbn: '978-1617933431',
       alreadyRead: true,
+      imgSrc :'assets/the_most_human_human.jpg',
     },
     {
       title: 'The Pragmatic Programmer',
       author: 'Andrew Hunt',
       isbn: '978-0201616224',
       alreadyRead: true,
+      imgSrc: 'assets/the_pragmatic_programmer.jpg',
     },
   ];
 
--- 2-Browsers/Week1/assignment/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/assignment/ex4-whatsTheTime/index.js
@@ -1,13 +1,36 @@
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-Browsers/Week1#exercise-4-whats-the-time
 
-1. Inside the `index.js`, complete the `addCurrentTime` to add the current time 
-  to the webpage. Make sure it's written in the HH:MM:SS notation (hour, minute,
-  second). Use `setInterval()` to make sure the time stays current.
-2. Have the function execute when it's loading in the browser.
-------------------------------------------------------------------------------*/
-function addCurrentTime() {
-  // TODO complete this function
-}
+  function addCurrentTime() {
 
-// TODO execute `addCurrentTime` when the browser has completed loading the page
+
+
+    const timeEl = document.createElement('p');
+    document.body.appendChild(timeEl);
+
+    function updateTime(){
+
+      const currentTime = new Date();
+      
+      let hours = currentTime.getHours();
+      let minutes = currentTime.getMinutes();
+      let seconds = currentTime.getSeconds();
+
+
+      hours = hours < 10 ? '0' + hours : hours ;
+      minutes = minutes < 10 ? '0 '+ minutes : minutes ;
+      seconds = seconds < 10 ? '0' + seconds : seconds ;
+
+
+
+      const timeFormat = `${hours}:${minutes}:${seconds}`;
+      timeEl.textContent = timeFormat;
+
+      }
+      setInterval(updateTime,1000);
+
+      updateTime();
+
+
+  }
+  window.addEventListener('load', addCurrentTime)
+
+
